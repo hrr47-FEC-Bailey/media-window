5bd324754fdd5ed3e0fc9300a476db4d6097f6bb
Tyler Jones <tejones1102@gmail.com>
2020-08-06 12:10:19 -0700

    Created DB/Server And Seeded DB.

---
+++ database/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/media-window', {useNewUrlParser: true});
+
+const gameSchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  description: String,
+  videoArr: Array,
+  photoArr: Array
+});
+
+const GameModel = mongoose.model('GameModel', gameSchema);
+
+
+const save = (data) => {
+  GameModel.create(data, (err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+  });
+}
+
+
+
+module.exports.save = save;
+module.exports.GameModel = GameModel;
---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoModel = require('../database/index');
+const app = express();
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//REMEMBER TO ADD STATIC PATH
+
+//REMEMBER TO FIX FIND ONE BELOW!!!!!!!!!!!!
+app.get('/videoData', (req, res) => {
+  mongoModel.GameModel.findOne({id: 1})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log('ERROR in get request: ', err)
+    });
+})
+
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
+
+
+
+
+//Ignore below. just an example of video layout
+
+{/* <iframe width="1280" height="720" src="https://www.youtube.com/embed/9Pip18Rzz3Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
