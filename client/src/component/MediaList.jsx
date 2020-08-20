cac26508302781fb2fcf99cabdb8ca2f0cb530c4
Tyler Jones <tejones1102@gmail.com>
2020-08-20 14:48:48 -0700

    Updated Buttons.

--- client/src/component/MediaList.jsx
+++ client/src/component/MediaList.jsx
@@ -1,82 +1,8 @@
 import React from 'react';
 import ImageListEntry from './ImageListEntry.jsx';
 import VideoListEntry from './VideoListEntry.jsx';
-import styled from 'styled-components';
 import styles from './styles.css';
 
-
-const Row = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  overflow: auto;
-  white-space: nowrap;
-  width: 600px;
-`;
-
-
-// const Row = createGlobalStyle`
-//   body {
-//     margin: 0;
-//     padding: 100px;
-//     background: #1B2838;
-//     display: flex;
-//     justify-content: flex-start;
-//     overflow-x: auto;
-//     overflow-y: hidden;
-//   }
-//   ::-webkit-scrollbar {
-//     width: 5px;
-//     height: 20px;
-//   }
-//   ::-webkit-scrollbar-track {
-//     border-radius: 3px;
-//     width: 5px;
-//     height: 20px;
-//   }
-//   ::-webkit-scrollbar-thumb {
-//     background: #07014 !important;
-//     border-radius: 3px;
-//   }
-//   ::-webkit-scrollbar-track {
-//     background: #16202D !important;
-//     border: 1px solid #16202D !important;
-//   }
-//   ::-webkit-scrollbar-thumb:hover {
-//     background: #B30000;
-//   }
-//   ::-webkit-scrollbar-button {
-//     background: #0B151F;
-//     background-size: 10px 10px;
-//     background-repeat: no-repeat;
-//     background-position: center center;
-//     -webkit-box-shadow: inset 1px 1px 2px rgba(0,0,0,0.2);
-//     width: 40px;
-//     border-radius: 5px;
-//   }
-//   ::-webkit-scrollbar-button:horizontal:decrement
-//   {
-//     border-width: 10px 20px 10px 0;
-//     border-color: transparent #407899 transparent transparent;
-//   }
-//   ::-webkit-scrollbar-button:horizontal:increment
-// {
-//   border-width: 10px 0 10px 20px;
-//   border-color: transparent transparent transparent #407899;
-// }
-// `;
-const Col = styled.div`
-  flex-direction: row;
-  vertical-align: middle;
-  flex-wrap: nowrap;
-  justify-content: flex-start;
-  height: 65px;
-  width: 116px;
-  background: transparent;
-  line-height: 65px;
-  margin: 2px, 2px, 2px, 0px;
-  overflow-x: hidden;
-  overflow-y: auto;
-`;
 
 const MediaList = (props) => (
   <div className={styles.row}>
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const corsOptions = {
   optionsSuccess: 200,
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true, useUnifiedTopology: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
