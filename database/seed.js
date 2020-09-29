cc1b21e7de30b3fb9ef84e7ceb7e06285aead69e
Tyler Jones <tejones1102@gmail.com>
2020-09-29 08:50:51 -0700

    Updated Seed.js.

--- database/seed.js
+++ database/seed.js
@@ -2,13 +2,11 @@ const faker = require('faker');
 const mongoModel = require('../database/index');
 
 
-
-
 function generateData() {
   let games = [];
   var videos = ["https://www.youtube.com/embed/JSRtYpNRoN0", "https://www.youtube.com/embed/X1p-_CNtL9w", "https://www.youtube.com/embed/TUHJ3ofLDs8", "https://www.youtube.com/embed/21hG1oZwQjk", "https://www.youtube.com/embed/RfLPGMOqg98", "https://www.youtube.com/embed/xXiOtv8I2Jk", "https://www.youtube.com/embed/xTpr7piQu2M", "https://www.youtube.com/embed/Jmliox1trPQ", "https://www.youtube.com/embed/Fmdb-KmlzD8", "https://www.youtube.com/embed/a3ZGGIdpfEM", "https://www.youtube.com/embed/EV2J1YPNnjo", "https://www.youtube.com/embed/2gUtfBmw86Y", "https://www.youtube.com/embed/q4GdJVvdxss", "https://www.youtube.com/embed/8pR0o2fGyHg"];
   const random = () => Math.floor(Math.random() * videos.length);
-  let imageId = 8;
+  let imageId = 10;
   for (let id = 1; id <= 100; id++) {
     let index = random();
     let title = faker.commerce.productName();
@@ -21,7 +19,7 @@ function generateData() {
     let i = 0;
     let j = 0;
     while (i < 6) {
-      let photo = `https://fecbailey.s3-us-west-1.amazonaws.com/image${imageId}.jpeg`
+      let photo = `https://fecbailey.s3-us-west-1.amazonaws.com/image${imageId}.jpg`
       photoArr.push(photo);
       i++;
       imageId++;
